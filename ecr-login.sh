0b213e1d33bb528c3664e663f46af01d9a7fca99
Andrew Cisternino <aacister@gmail.com>
2026-01-31 15:06:31 -0500

    Removing unnecessary scripts

--- ecr-login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ACCOUNT_ID="425865275846"
-REGION="us-east-2"
-REGISTRY="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
-
-aws ecr get-login-password --region "${REGION}" \
-  | docker login --username AWS --password-stdin "${REGISTRY}"
-
-echo "Logged in to ${REGISTRY}"
